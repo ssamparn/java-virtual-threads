c4a3fc0d7b09194f4d82e84045885e45347796d1
Sashank <sashanka.samantray@rabobank.nl>
2025-11-22 23:35:07 +0100

    java 25 upgrade

--- java-virtual-threads-trip-advisor-app/src/main/java/com/java/programming/javavirtualthreadstripadvisorapp/dto/tripplanning/Weather.java
+++ java-virtual-threads-trip-advisor-app/src/main/java/com/java/programming/javavirtualthreadstripadvisorapp/dto/tripplanning/Weather.java
@@ -1,6 +1,6 @@
 package com.java.programming.javavirtualthreadstripadvisorapp.dto.tripplanning;
 
 // Response object for Weather Service
-public record Weather(int temprature,
+public record Weather(Integer temperature,
                       String conditions) {
 }
